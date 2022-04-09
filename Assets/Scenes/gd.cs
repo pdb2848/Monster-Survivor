8611918ad82e516404eef702ac0c8a55e677e733
Tonarion7 <vitaminws@gmail.com>
2022-04-09 11:45:49 +0900

    recent commit

--- Assets/Scenes/gd.cs
+++ Assets/Scenes/gd.cs
@@ -2,12 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Move : MonoBehaviour
+public class gd : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("1111");
+        
     }
 
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
 }
